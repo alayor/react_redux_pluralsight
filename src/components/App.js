965bff9db0ac7bd32073a28cd5ebcc5b3f569522
alayor <alayor3@gmail.com>
2019-09-19 14:26:07 -0600

    add header

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Header from './common/Header'
+import HomePage from './home/HomePage'
+import AboutPage from './about/AboutPage'
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+    </div>
+  )
+}
+
+export default App
