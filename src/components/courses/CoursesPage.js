989d73506a6888969f0732dcbd38486a3d0b1b7c
alayor <alayor3@gmail.com>
2019-09-22 09:10:02 -0600

    create inital state for reducers

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -8,12 +8,17 @@ import CourseList from './CourseList'
 
 class CoursesPage extends React.Component {
   componentDidMount() {
-    this.props.actions.loadCourses().catch((error) => {
-      alert('Loading courses failed: ' + error)
-    })
-    this.props.actions.loadAuthors().catch((error) => {
-      alert('Loading authors failed: ' + error)
-    })
+    const { courses, authors, actions } = this.props
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert('Loading courses failed: ' + error)
+      })
+    }
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert('Loading authors failed: ' + error)
+      })
+    }
   }
 
   render() {
