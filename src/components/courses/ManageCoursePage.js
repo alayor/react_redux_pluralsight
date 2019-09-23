bbbbd97f2ec26df026837ce1a06d8510d2e79313
alayor <alayor3@gmail.com>
2019-09-22 19:10:37 -0600

    add ManageCoursePage

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { loadCourses as notCallThisLoadCourses } from '../../redux/actions/courseActions'
 import { loadAuthors as notCallThisLoadAuthors } from '../../redux/actions/authorActions'
 import PropTypes from 'prop-types'
+import CourseForm from './CourseForm'
+import { newCourse } from '../../../tools/mockData'
 
-function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors, ...props }) {
+  const [course, setCourse] = useState({ ...props.course })
+  const [errors, setErrors] = useState({})
   useEffect(async () => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
@@ -19,14 +23,11 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
       }
     }
   }, []) //the empty array means the effect will run once when the component mounts
-  return (
-    <>
-      <h2>Manage Course</h2>
-    </>
-  )
+  return <CourseForm course={course} errors={errors} authors={authors} />
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
@@ -35,6 +36,7 @@ ManageCoursePage.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors,
   }
