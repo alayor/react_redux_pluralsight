8fde853089b5ac4585bfff06a32fa4456ca1a01e
alayor <alayor3@gmail.com>
2019-09-24 22:09:33 -0600

    .

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -22,6 +22,8 @@ function ManageCoursePage({
       loadCourses().catch((error) => {
         alert('Loading courses failed: ' + error)
       })
+    } else {
+      setCourse({ ...props.course })
     }
     if (authors.length === 0) {
       try {
@@ -32,7 +34,7 @@ function ManageCoursePage({
         alert('Loading authors failed: ' + error)
       }
     }
-  }, []) //the empty array means the effect will run once when the component mounts
+  }, [props.course]) //an empty array means the effect will run once when the component mounts
 
   function handleChange(event) {
     const {
@@ -74,9 +76,15 @@ ManageCoursePage.propTypes = {
   history: PropTypes.object.isRequired,
 }
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug
+  const course = slug && state.courses.length ? getCourseBySlug(state.courses, slug) : newCourse
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors,
   }
